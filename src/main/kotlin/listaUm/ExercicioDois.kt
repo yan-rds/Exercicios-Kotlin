e65e1ab945555ad41011e4cd65196ba04430f233
yan-rds <yyan1207@gmail.com>
2022-02-12 23:24:52 -0300

    Lista 1 - Exercicio 2

---
+++ src/main/kotlin/listaUm/ExercicioDois.kt
@@ -0,0 +1,29 @@
+package listaUm
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+    fun main(){
+        val notas:ArrayList<Double> = ArrayList()
+        val scan = Scanner(System.`in`)
+        var contador = 1
+
+        while (contador <= 4) {
+            notas.add(scan.nextDouble())
+            contador++
+        }
+
+        println("Sua média é de ${getMedia(notas)}")
+    }
+
+    fun getMedia(notas:ArrayList<Double>) : Double{
+        var total = 0.0
+        for (nota in notas)
+            total += nota
+        val media = total / 4;
+        return media
+    }
+
+
+
