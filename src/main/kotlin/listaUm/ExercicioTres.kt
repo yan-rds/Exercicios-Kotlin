d6a143a20cceb6407afa300c371e9b4db2c6b37e
yan-rds <yyan1207@gmail.com>
2022-02-13 16:18:14 -0300

    Lista 1 - Exercicio 3

---
+++ src/main/kotlin/listaUm/ExercicioTres.kt
@@ -0,0 +1,16 @@
+package listaUm
+
+import java.util.*
+
+
+    fun main(){
+        val leitor = Scanner(System.`in`)
+        println("Conversor de metros em centímetros")
+        val metros:Double = leitor.nextDouble()
+        println("$metros metros equivale a ${conversor(metros)} centímetros")
+    }
+
+    fun conversor(metros:Double):Double{
+        val centimetros:Double = metros * 100
+        return centimetros
+    }
