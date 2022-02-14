969f1d66711951db04a64b93adfc873a2cb6068d
yan-rds <yyan1207@gmail.com>
2022-02-14 11:37:37 -0300

    Lista 3 - Exercicio 4

---
+++ src/main/kotlin/listaTres/ExercicioQuatro.kt
@@ -0,0 +1,21 @@
+package listaTres
+
+import listaUm.capturarDados
+
+fun main(){
+    val salario = capturarDados("Informe seu salário").nextDouble()
+    println("Seu novo salário será ${verificaAumento(salario)}")
+
+}
+
+fun verificaAumento(salario: Double): Double {
+    return if (salario <= 280)
+        salario * 1.2
+    else if (salario > 280 && salario <= 700)
+        salario * 1.15
+    else if (salario > 700 && salario <= 1500)
+        salario * 1.10
+    else
+        salario * 1.05
+
+}
