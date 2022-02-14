63a4623f9151c7935b2195c99075a98b8df33df4
yan-rds <yyan1207@gmail.com>
2022-02-14 11:25:41 -0300

    Lista 3 - Exercicio 3

---
+++ src/main/kotlin/listaTres/ExercicioTres.kt
@@ -0,0 +1,24 @@
+package listaTres
+
+import listaUm.capturarDados
+
+fun main(){
+    val a = capturarDados("Primeira nota").nextDouble()
+    val b = capturarDados("Segunda nota").nextDouble()
+    val media = calculaMedia(a, b)
+    verificaAprovacao(media)
+
+}
+
+fun calculaMedia(a:Double, b:Double):Double{
+    return (a + b) /2
+}
+
+fun verificaAprovacao(media:Double){
+    if (media >= 7 && media < 10)
+        println("Aprovado")
+    else if (media == 10.0)
+        println("Aprovado com distinção")
+    else
+        println("Reprovado")
+}
