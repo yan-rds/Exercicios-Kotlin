b73cb5ce4318ae81699b0e0aa8200d35429d4906
yan-rds <yyan1207@gmail.com>
2022-02-16 11:19:34 -0300

    Lista 4 - Exercicio 3

---
+++ src/main/kotlin/listaQuatro/ExercicioTres.kt
@@ -0,0 +1,35 @@
+package listaQuatro
+
+import listaUm.capturarDados
+import java.util.stream.Collectors
+
+fun main (){
+    analisaRespostas(interrogatorio())
+
+}
+
+fun interrogatorio():ArrayList<Boolean>{
+    val respostas:ArrayList<Boolean> = ArrayList()
+
+    respostas.add(capturarDados("Telefonou para a vítima?").nextBoolean())
+    respostas.add(capturarDados("Esteve no local do crime?").nextBoolean())
+    respostas.add(capturarDados("Mora perto da vítima?").nextBoolean())
+    respostas.add(capturarDados("Devia para a vítima?").nextBoolean())
+    respostas.add(capturarDados("Já trabalhou com a vítima?").nextBoolean())
+
+    return respostas
+
+}
+
+fun analisaRespostas(respostas:ArrayList<Boolean>){
+    val listaFiltrada:List<Boolean> = respostas.stream().filter { it == true }.collect(Collectors.toList())
+
+    when(listaFiltrada.size){
+        2 -> println("Suspeita")
+        3 -> println("Cumplice")
+        4 -> println("Cumplice")
+        5 -> println("Assassino")
+        else -> println("Inocente")
+    }
+
+}
