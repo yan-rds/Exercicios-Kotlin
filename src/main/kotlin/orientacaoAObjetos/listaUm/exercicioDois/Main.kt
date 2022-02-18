515b6d34b6e4b8437f901300fc7f40db4d3aedd2
Yan Rodrigues Santos <yyan1207@gmail.com>
2022-02-18 13:39:31 -0300

    Lista 1 - Exercicio 2 - POO

---
+++ src/main/kotlin/orientacaoAObjetos/listaUm/exercicioDois/Main.kt
@@ -0,0 +1,11 @@
+package orientacaoAObjetos.listaUm.exercicioDois
+
+fun main(){
+    val pessoa1 = Pessoa("yan", 22, 70.0, 1.72)
+    val pessoa2 = Pessoa("rodrigues", 22, null, 1.72)
+    val pessoa3 = Pessoa("santos", 22, altura = 1.72)
+    println(pessoa1)
+    println(pessoa2)
+    println(pessoa3)
+
+}
---
+++ src/main/kotlin/orientacaoAObjetos/listaUm/exercicioDois/Pessoa.kt
@@ -0,0 +1,3 @@
+package orientacaoAObjetos.listaUm.exercicioDois
+
+data class Pessoa (var nome:String, var idade:Int, var peso:Double? = 0.0, var altura:Double)
